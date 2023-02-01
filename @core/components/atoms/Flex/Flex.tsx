370b74a656592a5e71403a27e1b777f9ae2f3456
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:04:21 +0330

    create flex atom

---
+++ @core/components/atoms/Flex/Flex.tsx
@@ -0,0 +1,26 @@
+ 
+import React, { ReactNode } from 'react'
+import Styles from './Flex.module.scss'
+
+
+
+interface FlexProps extends React.AllHTMLAttributes<HTMLDivElement>{
+ variant?:'col_strt_cntr' | 'col_cntr_cntr' | 'cntr_cntr' | 'arnd_cntr' | 'col_cntr_strt' |'col_strt_strt'|'btwn_cntr'| 'col_cntr_btwn';
+}
+
+function Flex ({children, className,variant, ...rest}:FlexProps){
+  let classes = `${Styles.flex} ${className}`
+  
+  if(variant){
+   classes = `${Styles.flex} ${Styles[variant]} ${className}`
+  }
+ 
+ 
+  return (
+    <div className={classes} {...rest}>
+     {children}
+    </div>
+  )
+}
+
+export default Flex
