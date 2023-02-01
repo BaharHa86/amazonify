30df76fc266c7b4a1eb9375fae9ed286e83cb43e
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:08:48 +0330

    create link atom

---
+++ @core/components/atoms/LinkBox/LinkBox.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './LinkBox.module.scss'
+
+interface LinkBoxProps extends React.AnchorHTMLAttributes<HTMLAnchorElement>{
+ variant?:'nav_link' |'slider_link' | 'flex_link' | 'signin_condition';
+}
+
+ 
+function LinkBox({children,className,href='',variant, ...rest}:LinkBoxProps) {
+
+  let classes = `${styles.link_box} ${className}`;
+  
+  if(variant){
+   classes = `${styles[variant]} ${styles.link_box} ${className}`;
+  }
+ 
+  if(variant === 'nav_link'){ 
+    return (
+    <Link className={classes}  href={href}  {...rest}  >
+      {children}
+    </Link>
+   )}
+  
+   return (
+   <Link className={classes}  href={href}  {...rest}>
+   {children}
+   </Link>
+  )
+}
+
+export default LinkBox
+
+
+
+
+ 
