357ecfd09182815cafd7d5669c10d250ea12cf94
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:09:38 +0330

    create overlay atom

---
+++ @core/components/atoms/Overlay/Overlay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Overlay.module.scss';
+
+interface OvelayProps extends React.AllHTMLAttributes<HTMLDivElement> {
+ variant:'signin' | 'slider';
+}
+
+function Overlay({ className, variant }: OvelayProps) {
+  const overlayClasses = `${styles[variant]} ${className}`;
+  return <div className={overlayClasses} />;
+}
+
+export default Overlay;
