bb19d234ed799351f8cd8ce00c74c15ba7ad6149
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:07:58 +0330

    create input atom

---
+++ @core/components/atoms/InputFieldes/TextInput/TextInput.tsx
@@ -0,0 +1,23 @@
+ import React, { forwardRef } from 'react';
+ import styles from './TextInput.module.scss'
+
+interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+ variant:'search' | 'signin' | 'register' | 'cart';
+ 
+ 
+};
+
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  function TextInput({variant, className, ...rest}, ref) {
+     
+    const classes=`${styles.input} ${styles[variant]} ${className}`
+      
+        return(
+        <input  className={classes} {...rest} ref={ref} />
+        ) 
+  
+    }
+
+);
+
+export default TextInput;
