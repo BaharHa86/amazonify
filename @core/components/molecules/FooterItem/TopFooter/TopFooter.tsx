3396b47849189ff62de904a3237161c86f75b9e2
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:11:46 +0330

    create footer

---
+++ @core/components/molecules/FooterItem/TopFooter/TopFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../../../atoms/Button/Button'
+ 
+
+function TopFooter() {
+  return (
+    <Button variant='back_to_top' href='#'>Back to top</Button>
+  )
+}
+
+export default TopFooter
