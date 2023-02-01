51162a67fb7cf936b009684c216c68ee883c0f90
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:10:00 +0330

    create text atom

--- @core/components/atoms/Text/Text.tsx
+++ @core/components/atoms/Text/Text.tsx
@@ -1,43 +1,24 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
+import styles from './Text.module.scss'
  
 
-interface TextProps{
-children: ReactNode;
-className?:string;
-type?:string;
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement>{
+ 
+variant:'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6'| 'H7' | 'H8' |'H9' | 'body_xlarge' |'body_xlarge_2'|'body_large_1' |'body_large_2' | 'body_medium_1' |'body_medium_2'| 'body_small_1'|'body_small_2';
  
 }
 
-const Text: React.FC<TextProps> = ({children, className, type}) => {
-
-switch(type){
-
-  case 'p':
-    return <p className={className}>{children}</p> ;
-    break;
-  case 'h1':
-    return <h1 className={className}>{children}</h1> ;
-    break;
-  case 'h2':
-    return <h2 className={className}>{children}</h2> ;
-    break;
-  case 'h3':
-    return <h3 className={className}>{children}</h3> ;
-    break;
-  case 'h4':
-    return <h4 className={className}>{children}</h4> ;
-    break;
-  case 'h5':
-    return <h5 className={className}>{children}</h5> ;
-    break;
-  case 'h6':
-    return <h6 className={className}>{children}</h6> ;
-    break;
-  default:
-    return <div className={className}>{children}</div> ;
+ function Text({children,variant,className,...rest}:TextProps) {
+  const classes=`${className} ${styles[variant]}` ;
   
-}
-
+return(
+  
+  <p className={classes} {...rest}>{children}</p> 
+);
+    
+      
+     
+ 
 }
 
 export default Text
