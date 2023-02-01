e68a16eb470a528e733719f53718f9e5d932e20f
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:06:38 +0330

    create label atom

---
+++ @core/components/atoms/InputFieldes/Lable/Label.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Lable.module.scss'
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement>{
+  variant:'signin' | 'cart';
+  
+}
+function Label({children,className,variant,...rest}:LabelProps) {
+
+  const classes=`${className} ${styles.lable} ${styles[variant]}`
+  return (
+     <label className={classes} {...rest}>
+        {children}
+     </label>
+  )
+}
+
+export default Label
+
+
+ 
