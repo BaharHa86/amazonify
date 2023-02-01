478362ef51460e487eea671cf25a66723ccbc39b
BaharHa86 <119443389+BaharHa86@users.noreply.github.com>
2023-02-01 11:07:07 +0330

    create select atom

---
+++ @core/components/atoms/InputFieldes/Select/Select.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './select.module.scss'
+ 
+ 
+ 
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement>{
+  variant:'search';
+   title:string;
+   optionItems:string[];
+ }
+ 
+ 
+ function Select({title,optionItems,className,variant}:SelectProps) {
+  
+  const classes=`${styles.select} ${styles[variant]} ${className}`
+
+  return(
+      <select   className={classes} >
+        <option  value="">{title}</option>
+        {optionItems.map( (optionItem)=>{
+            return(
+        <option value={optionItem} key={optionItem}>{optionItem}</option>
+        )})}
+     
+        
+</select>
+ 
+
+    );
+
+
+
+   
+ }
+ 
+ export default Select
